cde611df2775ad9d92dd9954a59effb5f50b7d00
cory <darth22345@hotmail.com>
2018-03-07 20:25:30 +1100

    Finished the grabName function

--- scripts/function.js
+++ scripts/function.js
@@ -8,12 +8,15 @@ $(document).ready(function(){
 
 //A function that grabs the users name
 function nameGrab(){
-    name = $('#name').val();
+    name = $('#name').val(); //Storing the value of the box into a variable.
     console.log(name);
-    showHide()
+    showHide('#EnterName', 2000); //Calling the showHide function in order to toggle the state of the box.
+
+    document.getElementById('user').innerHTML = "Logged in as " + name; //injecting the user's name into the HTML
 }
 
 //A function that uses the jquery show and hide functions
 function showHide(Area, duration){      //Area is a string and Duration is a integer
     $(Area).toggle(duration);
 }
+
